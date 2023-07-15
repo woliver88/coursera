ac167eceed8a1528599320ffc18b4cad43400650
woliver88 <w.chaundy1988@gmail.com>
2023-07-15 15:03:08 +0100

    change to memory.h

--- course1/include/common/memory.h
+++ course1/include/common/memory.h
@@ -107,6 +107,6 @@ uint8_t *my_reverse(uint8_t *src, size_t length);
 
 int32_t *reserve_words(size_t length);
 
-void free_words(int32_t *src);
+void free_words(uint32_t *src);
 
 #endif /* __MEMORY_H__ */
