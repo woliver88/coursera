05cd4d649fdc8c914f4a081f182b0a6fd05f3161
woliver88 <w.chaundy1988@gmail.com>
2023-07-15 15:05:28 +0100

    comment out main from stats.c

--- course1/src/stats.c
+++ course1/src/stats.c
@@ -130,23 +130,23 @@ void print_statistics(unsigned char array[], unsigned int array_size)
 #endif
 }
 
-int main(void)
-{
+// int main(void)
+// {
 
-  unsigned char test[SIZE] = {34, 201, 190, 154, 8, 194, 2, 6,
-                              114, 88, 45, 76, 123, 87, 25, 23,
-                              200, 122, 150, 90, 92, 87, 177, 244,
-                              201, 6, 12, 60, 8, 2, 5, 67,
-                              7, 87, 250, 230, 99, 3, 100, 90};
+//   unsigned char test[SIZE] = {34, 201, 190, 154, 8, 194, 2, 6,
+//                               114, 88, 45, 76, 123, 87, 25, 23,
+//                               200, 122, 150, 90, 92, 87, 177, 244,
+//                               201, 6, 12, 60, 8, 2, 5, 67,
+//                               7, 87, 250, 230, 99, 3, 100, 90};
 
-  /* Other Variable Declarations Go Here */
+//   /* Other Variable Declarations Go Here */
 
-  /* Statistics and Printing Functions Go Here */
-  print_array(test, SIZE);
-  sort_array(test, SIZE);
-  print_array(test, SIZE);
-  print_statistics(test, SIZE);
-  return 0;
-}
+//   /* Statistics and Printing Functions Go Here */
+//   print_array(test, SIZE);
+//   sort_array(test, SIZE);
+//   print_array(test, SIZE);
+//   print_statistics(test, SIZE);
+//   return 0;
+// }
 
 /* Add other Implementation File Code Here */
